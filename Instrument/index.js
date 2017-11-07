236e4084bdea36e0665e41f4515dde90f9075edb
Mingshi Wu <miwu11@s.sfusd.edu>
2017-11-06 17:28:06 -0800

    hi

---
+++ Instrument/index.js
@@ -0,0 +1,41 @@
+var cNote = document.getElementById('cAudio');
+
+$('#c').mousedown(function(){
+    cNote.play();
+});
+
+var dNote = document.getElementById('dAudio');
+
+$('#d').mousedown(function(){
+    dNote.play();
+});
+
+var eNote = document.getElementById('eAudio');
+
+$('#e').mousedown(function(){
+    eNote.play();
+});
+
+var fNote = document.getElementById('fAudio');
+
+$('#f').mousedown(function(){
+    fNote.play();
+});
+
+var gNote = document.getElementById('gAudio');
+
+$('#g').mousedown(function(){
+    gNote.play();
+});
+
+var aNote = document.getElementById('aAudio');
+
+$('#a').mousedown(function(){
+    aNote.play();
+});
+
+var bNote = document.getElementById('bAudio');
+
+$('#b').mousedown(function(){
+    bNote.play();
+});
